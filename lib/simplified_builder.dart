829f3aea8db88d4a2a0597affe0a093f98a3ba52
Suhail Thakrani <57296007+suhailthakrani@users.noreply.github.com>
2023-03-07 11:21:03 +0500

    first commit

---
+++ lib/simplified_builder.dart
@@ -0,0 +1,4 @@
+library simplified_builder;
+
+// export 'src/simplified_future_builder.dart';
+// export 'src/simplified_stream_builder.dart';
---
+++ test/simplified_builder_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:simplified_builder/simplified_builder.dart';
+
+void main() {
+  test('adds one to input values', () {});
+}
